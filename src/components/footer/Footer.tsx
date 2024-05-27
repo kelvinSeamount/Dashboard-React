bc1f4e7211ad8f98bb3857fb8659ce5d474e6a38
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2024-05-28 00:36:11 +0200

    style footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>Meka Dashboard</span>
+      <span>Copyright © 2080</span>
+    </div>
+  );
 };
 
 export default Footer;
