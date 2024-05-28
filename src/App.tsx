304d0081ced2e435a15251d92bfc90aed870045b
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2024-05-28 15:11:20 +0200

    refactor layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./layouts/layout";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
 import Home from "./pages/home/Home";
 import Users from "./pages/users/Users";
 import Products from "./pages/product/Products";
 import "./styles/global.scss";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu
+              id={0}
+              title={""}
+              listItems={[
+                {
+                  id: 1,
+                  title: "",
+                  url: "",
+                  icon: "",
+                },
+              ]}
+            />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
   const router = createBrowserRouter([
     {
       path: "/",
